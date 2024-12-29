7677ca1acfc30323fbc34373943fa58a41ddcd31
Jan Höppner <hoeppner.jan@xurv.org>
2024-12-29 14:10:07 +0100

    neovim: Fix vim diagnostic settings
    'source' is a boolean, set it to true to always include the diagnostic
    source.
    
    Signed-off-by: Jan Höppner <hoeppner.jan@xurv.org>

--- neovim/.config/nvim/lua/core/lazy/lsp.lua
+++ neovim/.config/nvim/lua/core/lazy/lsp.lua
@@ -100,7 +100,7 @@ return {
                 focusable = false,
                 style = "minimal",
                 border = "rounded",
-                source = "always",
+                source = true,
                 header = "",
                 prefix = "",
             },
